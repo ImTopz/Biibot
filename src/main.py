03c2db9fec3170ecfff3edebe7fcdae8fcf7cf0c
ImTopz <1976521709@qq.com>
2022-09-07 12:44:05 +0800

    zdd-dev init

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env python3 
-from flask import Flask
-import click
+#coding=utf-8
+from flask import Flask,request
+import goapi
+import randomdsa
+app=Flask(__name__)
+@app.route('/',methods=['POST'])
+def getEvent():
+    data=request.json
+    if(data.get('post_type')=='message'):
+        message_type=data.get('message_type')
+        msg=data.get('message')
+        user_id=data.get('user_id')
+        if(message_type=='private'):
+            print(f"接收消息@{user_id}:{msg[:20]}")
+        else:
+            goapi.sendMsg(user_id,"抱歉,您不是好友,无法使用此功能")
 
-app = Flask(__name__)
-@app.route('/')
-def root():
-   return "Hello World"
 
-@click.command()
-@click.option('--host', default='localhost', help='set hostname to listen on')
-@click.option('--port', default=5000, help='set service port')
+    elif(data.get('post_type')=='request'):
+        pass
 
-def main(host, port):
-    """Accounting bot based on gocq-http and flask"""
-    app.run(host, port)
+
+
+    else:
+        pass
+
+    return 'OK'
+
+
 
 if __name__ == '__main__':
-    main()
+    app.run(host='127.0.0.1',port='5701',debug=False)
